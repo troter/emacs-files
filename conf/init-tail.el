12091639051b1f5d00844a1b28df572c21e94e96
takumi <trot.thunder@gmail.com>
2009-09-15 11:32:33 +0900

    add tail.el.

---
+++ conf/init-tail.el
@@ -0,0 +1,5 @@
+;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
+
+;; (auto-install-from-url "http://www.grassouille.org/code/sources/tail.el")
+(autoload 'tail-file "tail" "Tail a file." t)
+(autoload 'tail-command "tail" "Tail a file with arguments." t)
