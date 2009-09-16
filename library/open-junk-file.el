50fc125decbc8c02b53c917364b5189b2b23c5ec
イイノ タクミ <trot.thunder@gmail.com>
2009-09-16 21:52:55 +0900

    add open-junk-file.el

---
+++ library/open-junk-file.el
@@ -0,0 +1,12 @@
+;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
+
+;; refer: http://d.hatena.ne.jp/rubikitch/20080923/1222104034
+(defun open-junk-file ()
+  (interactive)
+  (let* ((file (expand-file-name
+                (format-time-string
+                 "%Y/%m/%Y-%m-%d-%H%M%S." (current-time))
+                "~/memo/junk/"))
+         (dir (file-name-directory file)))
+    (make-directory dir t)
+    (find-file-other-window (read-string "Junk Code: " file))))
