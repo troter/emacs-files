f41872ef1c66cc0d7b8800338e347e8b29a6b050
イイノ タクミ <trot.thunder@gmail.com>
2009-09-24 00:05:45 +0900

    anything-c-moccurの設定を追加

--- conf/init-anything.el
+++ conf/init-anything.el
@@ -5,7 +5,6 @@
 (when (require 'anything)
   (require 'anything-config)
   (require 'anything-match-plugin)
-
   (setq anything-su-or-sudo "sudo")
 
   ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/ac-anything.el")
@@ -79,9 +78,20 @@
        anything-c-source-file-name-history)
      "anything for buffers"))
 
-  (defun anything-kill-ring ()
-    (interactive)
-    (anything 'anything-c-source-kill-ring nil nil nil nil "*anything kill ring*"))
+  ;; (auto-install-from-url "http://svn.coderepos.org/share/lang/elisp/anything-c-moccur/trunk/anything-c-moccur.el")
+  (when (require 'anything-c-moccur)
+    ;; カスタマイズ可能変数の設定(M-x customize-group anything-c-moccur でも設定可能)
+    (setq anything-c-moccur-anything-idle-delay 0.2 ;`anything-idle-delay'
+          anything-c-moccur-higligt-info-line-flag t ; `anything-c-moccur-dmoccur'などのコマンドでバッファの情報をハイライトする
+          anything-c-moccur-enable-auto-look-flag t ; 現在選択中の候補の位置を他のwindowに表示する
+          anything-c-moccur-enable-initial-pattern t) ; `anything-c-moccur-occur-by-moccur'の起動時にポイントの位置の単語を初期パターンにする
+
+    ;;; キーバインドの割当(好みに合わせて設定してください)
+    (global-set-key (kbd "M-o") 'anything-c-moccur-occur-by-moccur) ;バッファ内検索
+    (global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur) ;ディレクトリ
+    (add-hook 'dired-mode-hook ;dired
+              '(lambda ()
+                 (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur))))
 
   ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/anything-complete.el")
   (when (require 'anything-complete)
@@ -104,4 +114,11 @@
     
     (define-key (anything-read-file-name-map) "\C-i" 'anything-select-action)
     )
+
+  ;; replacement for iswitchb.
+  (global-set-key [(control x) (b)] 'anything-my-for-buffers)
+  ;; replacement for yank-pop
+  (global-set-key [(meta y)] 'anything-show-kill-ring)
+  ;; replacement for query-query-replace
+  (global-set-key [(meta %)] 'anything-query-replace-regexp)
   )
