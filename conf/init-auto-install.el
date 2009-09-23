d17a1ec623ea012c3cf05e43576685f1b0a93d46
イイノ タクミ <trot.thunder@gmail.com>
2009-09-24 00:09:29 +0900

    パッケージを更新しない

--- conf/init-auto-install.el
+++ conf/init-auto-install.el
@@ -2,4 +2,4 @@
 ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/auto-install.el")
 (require 'auto-install)
 (setq auto-install-directory (concat plugins-directory "/"))
-(auto-install-update-emacswiki-package-name nil)
+;(auto-install-update-emacswiki-package-name nil)
