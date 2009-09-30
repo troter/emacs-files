ec78965d165c92e9141fc0f958e40cf3d7ce4383
takumi <trot.thunder@gmail.com>
2009-09-30 13:47:52 +0900

    defun-add-hookを利用する

--- conf/init-term.el
+++ conf/init-term.el
@@ -2,12 +2,12 @@
 
 ;; term
 ;; ----
-(add-hook 'term-mode-hook '(lambda ()
-                             (define-key term-raw-map "\C-z"
-                               (lookup-key (current-global-map) "\C-z"))
-                             (define-key term-raw-map "\C-p" 'previous-line)
-                             (define-key term-raw-map "\C-n" 'next-line)
-                             (term-set-escape-char ?\C-x)))
+(defun-add-hook 'term-mode-hook
+  (define-key term-raw-map "\C-z"
+    (lookup-key (current-global-map) "\C-z"))
+  (define-key term-raw-map "\C-p" 'previous-line)
+  (define-key term-raw-map "\C-n" 'next-line)
+  (term-set-escape-char ?\C-x))
 
 ;; shell
 ;; -----
