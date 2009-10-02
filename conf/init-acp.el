3662778ff71a4e99bf215a49e3af1275f42a9569
takumi <trot.thunder@gmail.com>
2009-10-02 20:50:00 +0900

    acp.elを追加

---
+++ conf/init-acp.el
@@ -0,0 +1,25 @@
+;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
+
+(require 'acp)
+
+;; refer: http://d.hatena.ne.jp/kitokitoki/20090823
+(add-hook 'emacs-lisp-mode-hook 'acp-mode)
+(add-hook 'lisp-mode-hook 'acp-mode)
+
+(setq acp-paren-alist
+  '((?( . ?))
+    (?[ . ?])))
+
+(setq acp-insertion-functions
+   '((mark-active . acp-surround-with-paren)
+     ((thing-at-point 'symbol) . acp-surround-symbol-with-paren)
+     (t . acp-insert-paren)))
+
+(defun acp-surround-symbol-with-paren (n)
+  (save-excursion
+    (save-restriction
+      (narrow-to-region (car (bounds-of-thing-at-point 'symbol)) (cdr (bounds-of-thing-at-point 'symbol)))
+      (goto-char (point-min))
+      (insert-char (car (acp-current-pair)) n)
+      (goto-char (point-max))
+      (insert-char (cdr (acp-current-pair)) n))))
