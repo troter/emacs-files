df51d0e98f539ecdaf475c90a4d06ca8b094a0c1
イイノ タクミ <trot.thunder@gmail.com>
2009-10-04 00:12:14 +0900

    key-chordの設定を分離

---
+++ conf/init-key-chord.el
@@ -0,0 +1,13 @@
+;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
+
+;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/key-chord.el")
+(when (require 'key-chord nil t)
+  (key-chord-mode 1)
+  (setq key-chord-two-keys-delay 0.1)
+  (setq key-chord-one-keys-delay 0.1)
+  ;; don't hijack input method!
+  (defadvice toggle-input-method (around toggle-input-method-around activate)
+    (let ((input-method-function-save input-method-function))
+      ad-do-it
+      (setq input-method-function input-method-function-save)))
+)
