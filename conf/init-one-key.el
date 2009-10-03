fbf7a223cf2774d3f1b891b79bab90df240a1bb8
イイノ タクミ <trot.thunder@gmail.com>
2009-10-04 00:11:04 +0900

    one-keyを追加

---
+++ conf/init-one-key.el
@@ -0,0 +1,10 @@
+;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
+
+;; (auto-install-from-url "http://www.emacswiki.org/cgi-bin/wiki/download/one-key.el")
+;; (auto-install-from-url "http://www.emacswiki.org/cgi-bin/wiki/download/one-key-config.el")
+;; (auto-install-from-url "http://www.emacswiki.org/cgi-bin/wiki/download/one-key-default.el")
+(when (require 'one-key-default)
+  (require 'one-key-config)
+  ;; one-key-make-templateが動かない。。。
+  ;;(one-key-default-setup-keys)
+  )
