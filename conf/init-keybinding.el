65bfae0b4e78265fd36c1c50e555897578b697d6
イイノ タクミ <trot.thunder@gmail.com>
2009-10-03 16:29:46 +0900

    コメント追加など

--- conf/init-keybinding.el
+++ conf/init-keybinding.el
@@ -1,14 +1,7 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 
-
-;(global-set-key "\C-z" 'undo)                       ;;UNDO
-(global-set-key [f1] 'help-for-help)
-
-(global-set-key [(control p)] (lambda () (interactive) (other-window -1)))
-
-(global-set-key [(control x) (control r)] 'reopen-file)
-(global-set-key [(control x) (J)] 'open-junk-file)
-
+;;; (@* "Global set keys")
+;; (@* "anything")
 (defun-eval-after-load 'anything
   ;; replacement for iswitchb.
   (global-set-key [(control x) (b)] 'anything-for-buffers)
@@ -31,9 +24,19 @@
   ;; directory
   (global-set-key [(control meta o)] 'anything-c-moccur-dmoccur))
 
+;; (@* "others")
 (defun-eval-after-load 'flymake
   (global-set-key [(control c) (d))] 'flymake-display-err-menu-for-current-line))
 
+;(global-set-key "\C-z" 'undo)                       ;;UNDO
+(global-set-key [f1] 'help-for-help)
+
+(global-set-key [(control p)] (lambda () (interactive) (other-window -1)))
+
+(global-set-key [(control x) (control r)] 'reopen-file)
+(global-set-key [(control x) (J)] 'open-junk-file)
+
+;;; (@* "key-chord")
 ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/key-chord.el")
 (when (require 'key-chord nil t)
   (key-chord-mode 1)
