d774d1591a9fac72902bd207796aeb0a9c9b9a35
イイノ タクミ <trot.thunder@gmail.com>
2009-10-17 19:15:07 +0900

    少し改良

--- conf/init-auto-complete.el
+++ conf/init-auto-complete.el
@@ -11,19 +11,18 @@
   (define-key ac-complete-mode-map [(tab)] 'ac-complete)
   (define-key ac-complete-mode-map [(return)] nil)
 
-  (eval-after-load "anything"
+  (defun-eval-after-load 'anything
     ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/ac-anything.el")
-    '(progn
-       (require 'ac-anything)
-       (define-key ac-complete-mode-map [(meta i)] 'ac-complete-with-anything)))
+    (require 'ac-anything)
+    (define-key ac-complete-mode-map [(meta i)] 'ac-complete-with-anything))
 
-  (eval-after-load "ruby-mode"
-    '(defun-add-hook 'ruby-mode-hook
-       (when (require 'rcodetools nil t)
-         (require 'auto-complete-ruby)
-         (make-local-variable 'ac-omni-completion-sources)
-         (setq ac-omni-completion-sources
-               '(("\\.\\=" . (ac-source-rcodetools)))))))
+  (defun-eval-after-load 'ruby-mode
+    (defun-add-hook 'ruby-mode-hook
+      (when (require 'rcodetools nil t)
+        (require 'auto-complete-ruby)
+        (make-local-variable 'ac-omni-completion-sources)
+        (setq ac-omni-completion-sources
+              '(("\\.\\=" . (ac-source-rcodetools)))))))
 
   (defun-add-hook 'emacs-lisp-mode-hook
     (require 'auto-complete-emacs-lisp)
@@ -31,4 +30,3 @@
     (setq ac-omni-completion-sources
           '(("\\.\\=" . (ac-source-emacs-lisp-features)))))
 )
-
