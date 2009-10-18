f24fe0caa86440a5df002f82c05ebdf4a11d0d25
イイノ タクミ <trot.thunder@gmail.com>
2009-10-18 23:58:06 +0900

    タブ削除

--- conf/init-font.el
+++ conf/init-font.el
@@ -18,16 +18,16 @@
    (carbon-p)
    (t
     (dolist (weight '(((name . "medium") (suffix . "r"))
-		      ((name . "bold") (suffix . "b"))))
+                      ((name . "bold") (suffix . "b"))))
       (dolist (size '("10" "12"))
-	(create-fontset-from-fontset-spec
-	 (funcall (compose
-		   (lambda (s) (replace-regexp-in-string "%suffix%" (cdr (assoc 'suffix weight)) s))
-		   (lambda (s) (replace-regexp-in-string "%weight%" (cdr (assoc 'name weight)) s))
-		   (lambda (s) (replace-regexp-in-string "%size%" size s)))
-		  (concat
-		   "-mplus-*-mplus-r-normal--%size%-*-*-*-*-*-fontset-mplus_%size%%suffix%,"
-		   "ascii:-mplus-gothic-%weight%-r-normal--%size%-*-iso8859-1,"
-		   "japanese-jisx0208:-mplus-gothic-%weight%-r-normal--%size%-*-jisx0208.1990-0,"
-		   "katakana-jisx0201:-mplus-gothic-%weight%-r-normal--%size%-*-jisx0201.1976-0")))))
+        (create-fontset-from-fontset-spec
+         (funcall (compose
+                   (lambda (s) (replace-regexp-in-string "%suffix%" (cdr (assoc 'suffix weight)) s))
+                   (lambda (s) (replace-regexp-in-string "%weight%" (cdr (assoc 'name weight)) s))
+                   (lambda (s) (replace-regexp-in-string "%size%" size s)))
+                  (concat
+                   "-mplus-*-mplus-r-normal--%size%-*-*-*-*-*-fontset-mplus_%size%%suffix%,"
+                   "ascii:-mplus-gothic-%weight%-r-normal--%size%-*-iso8859-1,"
+                   "japanese-jisx0208:-mplus-gothic-%weight%-r-normal--%size%-*-jisx0208.1990-0,"
+                   "katakana-jisx0201:-mplus-gothic-%weight%-r-normal--%size%-*-jisx0201.1976-0")))))
     (set-default-font "fontset-mplus_12r"))))
