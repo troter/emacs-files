ef8e50c185df0f16e7c43fcf04b52f59f0b3e889
takumi <trot.thunder@gmail.com>
2009-10-20 14:55:16 +0900

    条件を厳しく

--- conf/init-key-chord.el
+++ conf/init-key-chord.el
@@ -3,8 +3,8 @@
 ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/key-chord.el")
 (when (require 'key-chord nil t)
   (key-chord-mode 1)
-  (setq key-chord-two-keys-delay 0.1)
-  (setq key-chord-one-keys-delay 0.1)
+  (setq key-chord-two-keys-delay 0.05)
+  (setq key-chord-one-keys-delay 0.05)
   ;; don't hijack input method!
   (defadvice toggle-input-method (around toggle-input-method-around activate)
     (let ((input-method-function-save input-method-function))
