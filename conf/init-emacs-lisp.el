2b5a496f4a097f4d5c9a9d172d2901db4d236152
イイノ タクミ <trot.thunder@gmail.com>
2009-11-03 18:13:21 +0900

    elispテスト用ライブラリ追加

--- conf/init-emacs-lisp.el
+++ conf/init-emacs-lisp.el
@@ -5,3 +5,6 @@
 (defun-eval-after-load 'auto-complete
   (require 'auto-complete-emacs-lisp)
   (ac-emacs-lisp-init))
+
+;; (auto-install-from-emacswiki "el-expectations.el")
+;; (auto-install-from-emacswiki "el-mock.el")
