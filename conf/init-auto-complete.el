b43cd9121000f5915acbc0fe7e3057c55d16361f
イイノ タクミ <trot.thunder@gmail.com>
2009-11-22 23:55:30 +0900

    日本語を含めない

--- conf/init-auto-complete.el
+++ conf/init-auto-complete.el
@@ -6,6 +6,14 @@
 
   (set-default 'ac-sources '(ac-source-yasnippet ac-source-abbrev ac-source-words-in-buffer))
 
+  ;; auto-complete.el の ac-source-words-in-buffer の候補に日本語を含む単語が含まれないようにする
+  ;; refer: http://d.hatena.ne.jp/IMAKADO/20090813/1250130343
+  (defadvice ac-candidate-words-in-buffer (after remove-word-contain-japanese activate)
+    (let ((contain-japanese (lambda (s) (string-match (rx (category japanese)) s))))
+      (setq ad-return-value
+            (remove-if contain-japanese ad-return-value))))
+
+
   ;; Use M-n/M-p to select candidates
   (define-key ac-complete-mode-map [(meta n)] 'ac-next)
   (define-key ac-complete-mode-map [(meta p)] 'ac-previous)
