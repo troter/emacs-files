94402ff31acde2f930955e52433ac2ff9de071bc
takumi <trot.thunder@gmail.com>
2010-01-07 10:30:07 +0900

    優先順位変更

--- conf/init-smartchr.el
+++ conf/init-smartchr.el
@@ -2,9 +2,9 @@
 
 ;; (auto-install-from-url "http://github.com/imakado/emacs-smartchr/raw/master/smartchr.el")
 (require 'smartchr)
-(global-set-key (kbd "=") (smartchr '(" = " " == " " === " "=")))
+(global-set-key (kbd "=") (smartchr '("=" " = " " == " " === ")))
 (global-set-key (kbd "{")
-                (smartchr '("{ `!!' }" "{ \"`!!'\" }" "{")))
+                (smartchr '("{" "{ `!!' }" "{ \"`!!'\" }")))
 (global-set-key (kbd ">") (smartchr '(">" " => " " => '`!!''" " => \"`!!'\"")))
 
 (defun-eval-after-load 'php-mode
