1f90179a125bc3b03fe9697656927f131acdf82b
IINO Takumi <trot.thunder@gmail.com>
2010-02-05 20:43:10 +0900

    emacs23用フォント設定追加

--- conf/init-font.el
+++ conf/init-font.el
@@ -3,18 +3,22 @@
 (when window-system
   (cond
    (windows-p
-    (setq w32-use-w32-font-dialog nil)
-    (setq scalable-fonts-allowed t)
-    (setq w32-enable-synthesized-fonts t)
-    (create-fontset-from-fontset-spec
-     (concat
-     "-*-Courier New-normal-r-*-*-12-*-*-*-c-*-fontset-TTG12c,"
-     "japanese-jisx0208:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-jisx0208-sjis,"
-     "latin-jisx0201:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-jisx0208-sjis,"
-     "katakana-jisx0201:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-jisx0208-sjis,"
-     "mule-unicode-e000-ffff:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-iso10646-1") t)
-    (setcdr (assoc 'font default-frame-alist) "fontset-default")
-    (set-frame-font "fontset-default"))
+    (cond
+     (emacs23-p
+      (set-default-font "M+2VM+IPAG circle-8"))
+     (t
+      (setq w32-use-w32-font-dialog nil)
+      (setq scalable-fonts-allowed t)
+      (setq w32-enable-synthesized-fonts t)
+      (create-fontset-from-fontset-spec
+       (concat
+        "-*-Courier New-normal-r-*-*-12-*-*-*-c-*-fontset-TTG12c,"
+        "japanese-jisx0208:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-jisx0208-sjis,"
+        "latin-jisx0201:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-jisx0208-sjis,"
+        "katakana-jisx0201:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-jisx0208-sjis,"
+        "mule-unicode-e000-ffff:-*-ＭＳ ゴシック-*-*-*-*-*-*-*-*-*-*-iso10646-1") t)
+      (setcdr (assoc 'font default-frame-alist) "fontset-default")
+      (set-frame-font "fontset-default"))))
    (carbon-p)
    (t
     (dolist (weight '(((name . "medium") (suffix . "r"))
