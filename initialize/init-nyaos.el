d7b4d58365c1809b6b394c56b30f96ad0cb7aa97
U-takumi-THINK\takumi <trot.thunder@gmail.com>
2010-12-26 02:36:30 +0900

    nyaosの設定どうしよう。。

--- initialize/init-nyaos.el
+++ initialize/init-nyaos.el
@@ -3,20 +3,21 @@
 (when (and windows-p
            (or cygwin-p
                (not (getenv "SHLVL"))))
-  ;;; WindowsNT に付属の CMD.EXE を使う場合。
-  (setq explicit-shell-file-name "CMDPROXY.EXE")
-  (setq shell-file-name "CMDPROXY.EXE")
-  (setq shell-command-switch "\\/c")
+  (defun tr:use-nyaos ()
+    ;;; WindowsNT に付属の CMD.EXE を使う場合。
+    (setq explicit-shell-file-name "CMDPROXY.EXE")
+    (setq shell-file-name "CMDPROXY.EXE")
+    (setq shell-command-switch "\\/c")
 
-  ;;; nyaosを利用する場合 暫定版
-  (setq nyaos.exe nil)
-  ;;(setq nyaos.exe
-  ;;      (or (executable-find "nyaos")
-  ;;          (executable-find (concat (getenv "NYAOS_HOME") "\\nyaos"))))
-  (when nyaos.exe
-    (setq explicit-shell-file-name nyaos.exe
-          shell-file-name explicit-shell-file-name
-          shell-command-switch ""
-          explicit-nyaos.exe-args '()
-          shell-file-name-chars "~/A-Za-z0-9_^$!#%&{}@`'.,:()-")
-    ))
+    ;;; nyaosを利用する場合 暫定版
+    (setq nyaos.exe nil)
+    ;;(setq nyaos.exe
+    ;;      (or (executable-find "nyaos")
+    ;;          (executable-find (concat (getenv "NYAOS_HOME") "\\nyaos"))))
+    (when nyaos.exe
+      (setq explicit-shell-file-name nyaos.exe
+            shell-file-name explicit-shell-file-name
+            shell-command-switch ""
+            explicit-nyaos.exe-args '()
+            shell-file-name-chars "~/A-Za-z0-9_^$!#%&{}@`'.,:()-")
+      )))
