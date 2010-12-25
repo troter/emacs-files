7d144c26c55bbb31d602b964026a18f860273b14
U-takumi-THINK\takumi <trot.thunder@gmail.com>
2010-12-25 20:54:58 +0900

    auto-complete settings

--- initialize/init-auto-complete.el
+++ initialize/init-auto-complete.el
@@ -2,46 +2,47 @@
 ;; 
 (when (require 'auto-complete-config nil t)
   (add-to-list 'ac-dictionary-directories (expand-file-name "auto-complete/ac-dict" plugins-directory))
-  (ac-config-default)
 
-;;  (setq ac-menu-height 20)
-;;  (setq ac-candidate-limit 10)
-;;  (setq ac-dwin t)
+  (setq ac-menu-height 20)
+  (setq ac-ignore-case 'smart)
+  (setq ac-dwin t)
 ;;
 ;;  ;; default sources
 ;;  ;;(set-default 'ac-sources '(ac-source-abbrev ac-source-words-in-buffer))
 ;;  ;;(when (ac-yasnippet-initialize)
 ;;  ;;  (set-default 'ac-sources '(ac-source-yasnippet ac-source-abbrev ac-source-words-in-buffer)))
 ;;
-;;  (defun-add-hook 'emacs-lisp-mode-hook (ac-emacs-lisp-features-initialize))
-;;  (defun-add-hook 'auto-complete-mode-hook (add-to-list 'ac-sources 'ac-source-filename))
-;;
 ;;  (defun-eval-after-load 'anything
 ;;    ;; (auto-install-from-url "http://www.emacswiki.org/emacs/download/ac-anything.el")
 ;;    (require 'ac-anything)
-;;    (define-key ac-complete-mode-map [(meta i)] 'ac-complete-with-anything))
-;;
-;;  ;; auto-complete.el の ac-source-words-in-buffer の候補に日本語を含む単語が含まれないようにする
-;;  ;; refer: http://d.hatena.ne.jp/IMAKADO/20090813/1250130343
-;;  (defadvice ac-candidate-words-in-buffer (after remove-word-contain-japanese activate)
-;;    (let ((contain-japanese (lambda (s) (string-match (rx (category japanese)) s))))
-;;      (setq ad-return-value
-;;            (remove-if contain-japanese ad-return-value))))
-;;
-;;  ;;; keybindings
-;;  (global-auto-complete-mode t)
-;;  (ac-set-trigger-key "TAB")
-;;
-;;  ;; Don't start completion automatically
-;;  (setq ac-auto-start nil)
-;;  (global-set-key "\M-/" 'auto-complete)
-;;
-;;  ;; Use C-n/C-p to select candidates
-;;  (define-key ac-complete-mode-map [(control n)] 'ac-next)
-;;  (define-key ac-complete-mode-map [(control p)] 'ac-previous)
+;;    (define-key ac-mode-map [(meta i)] 'ac-complete-with-anything))
+
+  ;;; keybindings
+  (ac-set-trigger-key "TAB")
+
+  ;; Don't start completion automatically
+  (setq ac-auto-start nil)
+  (global-set-key "\M-/" 'auto-complete)
+
+  ;; Use C-n/C-p to select candidates
+  (setq ac-use-menu-map t)
+  (define-key ac-menu-map [(control n)] 'ac-next)
+  (define-key ac-menu-map [(control p)] 'ac-previous)
+  ;;(define-key ac-complete-mode-map [(control n)] 'ac-next)
+  ;;(define-key ac-complete-mode-map [(control p)] 'ac-previous)
+
+  ;; Help
+  (define-key ac-mode-map (kbd "C-c h") 'ac-last-quick-help)
+  (define-key ac-mode-map (kbd "C-c H") 'ac-last-help)
+
+  ;; Stop completion
+  (define-key ac-completing-map "\M-/" 'ac-stop)
 ;;
 ;;  ;; Select candidates by TAB
 ;;  (define-key ac-complete-mode-map [(tab)] 'ac-expand)
 ;;  ;; Completion by RETURN
 ;;  (define-key ac-complete-mode-map [(return)] 'ac-complete)
+
+  ;; Enable auto-complete
+  (ac-config-default)
 )
