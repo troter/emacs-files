ea3ae7258ab06ab1445264328b24d6700d9941b1
U-takumi-THINK\takumi <trot.thunder@gmail.com>
2010-12-27 18:48:07 +0900

    elscreen の起動が速すぎてエラーが出るので遅らせる

--- initialize/init-elscreen.el
+++ initialize/init-elscreen.el
@@ -1,8 +1,9 @@
 ;;; -*- coding: utf-8; indent-tabs-mode: nil -*-
 
-(when (load-safe "elscreen")
-  (require 'elscreen-dired)
-  (define-key elscreen-map "\C-z" 'suspend-emacs)
-  (defun-eval-after-load 'anything-config
-    (define-key elscreen-map "\C-o" '(lambda () (interactive) (anything 'anything-c-source-elscreen)))))
+(defun-add-hook 'after-init-hook
+  (when (load-safe "elscreen")
+    (require 'elscreen-dired)
+    (define-key elscreen-map "\C-z" 'suspend-emacs)
+    (defun-eval-after-load 'anything-config
+    (define-key elscreen-map "\C-o" '(lambda () (interactive) (anything 'anything-c-source-elscreen))))))
 
